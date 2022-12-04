5b61125fb89a11a1b4f7239f919012c78f48fa4c
Елена Кливер <volandmart@mail.ru>
2022-12-04 17:53:42 +0500

    final

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV.java
@@ -0,0 +1,24 @@
+package ru.netology.rest;
+
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .contentType(ContentType.JSON)
+        ;
+    }
+}
